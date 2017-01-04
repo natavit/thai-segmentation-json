0592b83e52de0334539febb8c5892bc176527436
Nut Janekitiworapong <nutjane131@gmail.com>
2017-01-05 02:30:47 +0700

    Refactor code and Implement JSON put as a new method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Main {
+	public static String[] columnTitle = null;
+	public static String[] columns = null;
+	public static List<Integer> intList = null;
+
+	public static void put(JSONObject object, int i) {
+		if (intList.contains(i)) {
+			System.out.println(columns[i]);
+			if(columns[i].length()==0){
+				object.put(columnTitle[i], 0);
+			}else{
+				object.put(columnTitle[i], Integer.parseInt(columns[i].replaceAll(",", "")));
+			}
+		} else {
+			object.put(columnTitle[i], columns[i]);
+		}
+	}
 
 	public static void main(String[] args) {
 
+		Integer[] intDataType = { 0, 25, 41, 42, 48, 52 };
+		intList = Arrays.asList(intDataType);
+
 		String thisLine = null;
 		boolean firstLine = true;
-		String[] columnTitle = null;
+
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("r2r59-9.tsv"));
 			while ((thisLine = br.readLine()) != null) {
@@ -17,15 +39,15 @@ public class Main {
 					columnTitle = thisLine.split("	");
 					firstLine = false;
 				} else {
-					String[] columns = thisLine.split("	");
-					
+					columns = thisLine.split("	");
+
 					JSONObject object = new JSONObject();
 					// researcher
 					JSONArray list = new JSONArray();
 					for (int i = 3; i <= 8; i++) {
 						JSONObject obj = new JSONObject();
-						obj.put(columnTitle[i], columns[i]);
-						obj.put(columnTitle[i + 7], columns[i]);
+						put(obj, i);
+						put(obj, i + 7);
 						list.add(obj);
 					}
 					object.put("researcher", list);
@@ -34,6 +56,7 @@ public class Main {
 					try {
 						BufferedReader in = new BufferedReader(new FileReader("input"));
 						while ((inputLine = in.readLine()) != null) {
+							System.out.println(inputLine);
 							String[] inps = inputLine.split("-");
 							// o = object
 							if (inps[0].equals("o")) {
@@ -43,7 +66,8 @@ public class Main {
 									JSONObject obj = new JSONObject();
 									for (int i = Integer.parseInt(inps[2].trim()); i <= Integer
 											.parseInt(inps[3].trim()); i++) {
-										obj.put(columnTitle[i], columns[i]);
+
+										put(obj,i);
 									}
 									object.put(inps[1].trim().toLowerCase(), obj);
 								} else if (inps.length == 3) {
@@ -52,8 +76,7 @@ public class Main {
 									JSONObject obj = new JSONObject();
 									for (int i = 0; i < inpList.length; i++) {
 
-										obj.put(columnTitle[Integer.parseInt(inpList[i])],
-												columns[Integer.parseInt(inpList[i])]);
+										put(obj,Integer.parseInt(inpList[i]));
 									}
 									object.put(inps[1].trim().toLowerCase(), obj);
 								}
@@ -64,13 +87,15 @@ public class Main {
 									for (int i = Integer.parseInt(inps[1].trim()); i <= Integer
 											.parseInt(inps[2].trim()); i++) {
 										object.put(columnTitle[i], columns[i]);
+										put(object,i);
+
 									}
 								} else if (inps.length == 2) {
 									String[] inpList = inps[1].split(",");
 
 									for (int i = 0; i < inpList.length; i++) {
-										object.put(columnTitle[Integer.parseInt(inpList[i].trim())],
-												columns[Integer.parseInt(inpList[i].trim())]);
+
+										put(object, Integer.parseInt(inpList[i].trim()));
 									}
 								}
 							}
